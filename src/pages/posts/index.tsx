ebdf7013c9a867e1c083b7d6279b721788770042
José Assis de Menezes Neto <jose.neto@hitcode.com.br>
2021-05-04 13:41:19 -0300

    Finalizada a página de post e também a validação para verificar se o usuário possui uma assinatura ativa, agora trabalhar na página de preview de post

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Link from 'next/link';
 
 import styles from './styles.module.scss';
 import { GetStaticProps } from 'next';
@@ -27,11 +28,13 @@ export default function Posts({ posts }: PostProps) {
       <main className={styles.container}>
         <div className={styles.posts}>
           {posts.map((post) => (
-            <a key={post.slug} href="#">
-              <time>{post.updatedAt}</time>
-              <strong>{post.title}</strong>
-              <p>{post.excerpt}</p>
-            </a>
+            <Link key={post.slug} href={`/posts/${post.slug}`}>
+              <a>
+                <time>{post.updatedAt}</time>
+                <strong>{post.title}</strong>
+                <p>{post.excerpt}</p>
+              </a>
+            </Link>
           ))}
         </div>
       </main>
